686bfa79df9ce1eeccb177c875395cdab502879a
ShawE <devshawe@gmail.com>
2022-03-01 01:22:14 +0200

    final

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
 app.listen(800, function () {
   console.log("app is running");
 });
@@ -24,6 +27,7 @@ app.get("/user/:id", function (req, res) {
 });
 
 app.post("/login", function (req, res) {
+  console.log(req.params);
   const username = req.body.username;
   const password = req.body.password;
   const mockUsername = "billyTheKid";
